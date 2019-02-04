51915725ee49700b5387e596f3ddf7a902a05ab4
Olorunfemi Ajibulu <FzanyAjibs@gmail.com>
2019-02-04 18:58:31 +0100

    initial

---
+++ Encryp/Encryption.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Encryp
+{
+    public class Encryption
+    {
+        private static readonly byte[] BookriteKeyByteArray = Convert.FromBase64String("WLFwhMRRi95OfQ2/rJaD/rzhg3EJnuXvEbhRamPX3W8=");
+        private static readonly byte[] BookriteIvByteArray = Convert.FromBase64String("jCs36/TBH5MjB/hkXLa+9g==");
+
+         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public static string EncryptString(string plainText)
+        {
+            byte[] Key = BookriteKeyByteArray;
+            byte[] IV = BookriteIvByteArray;
+
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+            {
+                return string.Empty;
+            }
+
+            byte[] encrypted;
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            // Return the encrypted bytes from the memory stream.
+            return Convert.ToBase64String(encrypted);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="encryptedText"></param>
+        /// <returns></returns>
+        public static string DecryptString(string encryptedText)
+        {
+            byte[] cipherText = Convert.FromBase64String(encryptedText);
+            byte[] Key = BookriteKeyByteArray;
+            byte[] IV = BookriteIvByteArray;
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+            {
+                return string.Empty;
+            }
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        }
+    }
+
+}
--- Encryp/Program.cs
+++ Encryp/Program.cs
@@ -2,14 +2,154 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Encryp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
+            origin:
+            DateTime startDate = DateTime.UtcNow;
+            DateTime endDate = DateTime.UtcNow;
+            double counter = 0;
+            Console.WriteLine("Enter text to brute");
+            string match = Console.ReadLine();
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            string[] alphabeth = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                                    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+",
+                                    "[", "]", "{", "}", ";", ":", ",", "<", ">", "?", "/", " ", "|", @"\"};
+            string result = string.Empty;
+            bool reversed = false;
+            StringBuilder sob = new StringBuilder();
+            foreach (string item in alphabeth)
+            {
+                sob.Append(item);
+            }
+            again:
+          
+            //Console.WriteLine(sob.ToString());
+            string format = sob.ToString();
+          
+            int lenght = format.Length;
+            int starter = 0;
+            int lenghter = 1;
+            int ender = 0;
+            bool isMultiple = false;
+            start:
+            if (lenghter > (lenght - starter))
+            {
+                ender = lenght - starter;
+                isMultiple = true;
+            }
+            else
+            {
+                ender = lenghter;
+                isMultiple = false;
+            }
+            result = EndConverter(format.Substring(starter, ender), isMultiple, lenghter);
+            counter++;
+           
+            //Console.Clear();
+            // Console.WriteLine(counter);
+
+            if (result == match)
+            {
+                Console.WriteLine("match found");
+                Console.WriteLine(result);
+                endDate = DateTime.UtcNow;
+                Console.WriteLine($"Search started: {startDate.ToLongDateString()}, {startDate.ToLongTimeString()}");
+                Console.WriteLine($"Search finished: {endDate.ToLongDateString()}, {endDate.ToLongTimeString()}");
+                double minutesTaken = (endDate - startDate).TotalMinutes;
+                Console.WriteLine($"Search took: {minutesTaken} minutes and {counter} counts");
+
+                Console.WriteLine("Press any key to start again.");
+                Console.ReadKey();
+                goto origin;
+            }
+            else
+            {
+                Console.WriteLine(result);
+                starter++;
+                if (starter >= lenght)
+                {
+                    starter = 0;
+                    lenghter++;
+                }
+
+                if (lenghter <= lenght)
+                {
+                    goto start;
+                }
+                else
+                {
+                    if (!reversed)
+                    {
+                        reversed = true;
+                        alphabeth = alphabeth.Reverse().ToArray();
+                        sob.Clear();
+                        foreach (string item in alphabeth)
+                        {
+                            sob.Append(item);
+                        }
+                        goto again;
+                    }
+                    else
+                    {
+                        reversed = false;
+                        //scramble the list
+                        List<string> temp1 = new List<string>();
+                        while (temp1.Distinct().Count() != alphabeth.Count())
+                        {
+                            string ins = alphabeth[GetRandomNumber(0, alphabeth.Count())];
+                            if (!temp1.Contains(ins))
+                            {
+                                temp1.Add(ins);
+                            }
+                        }
+                        alphabeth = temp1.ToArray();
+                        sob.Clear();
+                        foreach (string item in alphabeth)
+                        {
+                            sob.Append(item);
+                        }
+                        goto again;
+                    }
+
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+        public static readonly Random getrandom = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+        private static string EndConverter(string v, bool isMultiple, int lenghter)
+        {
+            if (isMultiple)
+            {
+                string selected = v.Substring((v.Length - 1), 1);
+                StringBuilder sob = new StringBuilder();
+                sob.Append(v);
+                for (int j = 0; j < lenghter - v.Length; j++)
+                {
+                    sob.Append(selected.ToString());
+                }
+
+                return sob.ToString();
+            }
+            return v;
         }
     }
 }
